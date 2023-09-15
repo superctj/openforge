28862f4f5e5e9c6443312ff354e4968824470998
superctj <congtj@umich.edu>
2023-09-14 21:27:56 -0400

    script to dump icpsr data to mongodb

--- dump_icpsr_data.py
+++ dump_icpsr_data.py
@@ -17,6 +17,9 @@ if __name__ == "__main__":
         if os.path.isdir(study_dir):
             collection_name = f"{collection_prefix}_{study}"
             collection = db[collection_name]
+            if collection_name in db.list_collection_names():
+                collection.drop()
+            
             for file_name in os.listdir(study_dir):
                 if file_name.endswith(".tsv"):
                     file_path = os.path.join(study_dir, file_name)
@@ -24,11 +27,20 @@ if __name__ == "__main__":
                         table = pd.read_csv(file_path, delimiter="\t", on_bad_lines="skip")
                     except UnicodeDecodeError:
                         table = pd.read_csv(file_path, delimiter="\t", encoding ="ISO-8859-1", on_bad_lines="skip")
+                    except pd.errors.ParserError:
+                        table = pd.read_csv(file_path, delimiter="\t", engine="python", on_bad_lines="skip")
+                    except Exception as e:
+                        print(f"Error in reading table from {file_path}: {e}")
 
                     # Drop empty columns
                     table.dropna(axis="columns", how="all", inplace=True)
                     # Drop empty rows
                     table.dropna(axis="index", how="all", inplace=True)
             
-                    collection.insert_many(table.to_dict("records"))
+                    try:
+                        collection.insert_many(table.to_dict("records"))
+                    except Exception as e:
+                        # MongoDB can only handle up to 8-byte ints
+                        print(f"Error in inserting documents to MongoDB: {e} when processing {file_path}")
+                        continue
     client.close()
