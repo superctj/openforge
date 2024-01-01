27701a584de4a3db1df592ee4e8cdc0c6871bec3
superctj <congtj@umich.edu>
2024-01-01 05:31:10 +0000

    add util of finding project directory

---
+++ openforge/utils/util.py
@@ -0,0 +1,17 @@
+import os
+
+
+def get_proj_dir(filepath: str, file_level: int=3) -> str:
+    """Get the project directory.
+
+    Args:
+        filepath (str): The path of the file.
+        file_level (int): The level of the file relative to the project directory starting at 1.
+    
+    Returns:
+        (str): The project directory.
+    """
+
+    abs_path = os.path.abspath(filepath)
+    proj_dir = "/".join(abs_path.split("/")[:-file_level])
+    return proj_dir
