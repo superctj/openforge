e82f4f3af9e650799b53d18cb9ad3c28e4b56fdb
superctj <congtj@umich.edu>
2024-01-23 15:12:22 -0500

    complement the logic when a log filepath is passed to log_dir

--- openforge/utils/custom_logging.py
+++ openforge/utils/custom_logging.py
@@ -5,11 +5,11 @@ from datetime import datetime
 
 
 def get_custom_logger(log_dir: str, log_level=logging.INFO):
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    
-    cur_datetime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    log_filepath = os.path.join(log_dir, f"{cur_datetime}.log")
+    if not os.path.isdir(log_dir):
+        log_filepath = log_dir
+    else:
+        cur_datetime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        log_filepath = os.path.join(log_dir, f"{cur_datetime}.log")
 
     logger = logging.getLogger(log_filepath)
     logger.setLevel(log_level)
