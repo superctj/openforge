e1dc72395e7895833ddb8cf3f01b02decc4073e2
superctj <congtj@umich.edu>
2024-01-31 22:17:42 -0500

    support loading arts mrf data

--- openforge/train_prior_model.py
+++ openforge/train_prior_model.py
@@ -23,27 +23,50 @@ class RidgeClassifierwithProba(linear_model.RidgeClassifier):
         return extmath.softmax(d_2d)
 
 
-def prepare_sotab_data_for_inference(data_filepath: str):
-    if data_filepath.endswith(".pkl"):
-        with open(data_filepath, "rb") as f:
-            sotab_benchmark = pickle.load(f)
-            X_sotab = np.array([x[0] for x in sotab_benchmark])
-            y_sotab = np.array([x[1] for x in sotab_benchmark])
-    else:
-        assert data_filepath.endswith(".csv")
+def load_arts_data(data_filepath: str, args: argparse.Namespace):
+    arts_df = pd.read_csv(data_filepath, delimiter=",", header=0)
+    print("ARTS data shape: ", arts_df.shape)
+    y_arts_df = arts_df.pop("relation_variable_label")
 
-        sotab_df = pd.read_csv(data_filepath, delimiter=",", header=0)
+    X_train_df, X_test_df, y_train_df, y_test_df = train_test_split(
+        arts_df, y_arts_df,
+        train_size=args.arts_train_prop,
+        random_state=args.random_seed,
+        stratify=y_arts_df
+    )
 
-        X_sotab = []
-        y_sotab = []
+    X_train = X_train_df[["name_similarity", "value_similarity"]].to_numpy()
+    y_train = y_train_df.to_numpy()
 
-        for row in sotab_df.itertuples():
-            X_sotab.append([row.name_similarity, row.value_similarity])
-            y_sotab.append(row.relation_variable_label)
-        
-        X_sotab = np.array(X_sotab)
-        y_sotab = np.array(y_sotab)
+    X_valid_df, X_test_df, y_valid_df, y_test_df = train_test_split(
+        X_test_df, y_test_df,
+        test_size=0.5,
+        random_state=args.random_seed,
+        stratify=y_test_df
+    )
+
+    X_valid = X_valid_df[["name_similarity", "value_similarity"]].to_numpy()
+    y_valid = y_valid_df.to_numpy()
+
+    X_test = X_test_df[["name_similarity", "value_similarity"]].to_numpy()
+    y_test = y_test_df.to_numpy()
+
+    X_test_df["relation_variable_label"] = y_test_df
     
+    return X_train, y_train, X_valid, y_valid, X_test, y_test, X_test_df
+
+
+def load_sotab_data(data_filepath: str):
+    sotab_df = pd.read_csv(data_filepath, delimiter=",", header=0)
+    X_sotab, y_sotab = [], []
+
+    for row in sotab_df.itertuples():
+        X_sotab.append([row.name_similarity, row.value_similarity])
+        y_sotab.append(row.relation_variable_label)
+    
+    X_sotab = np.array(X_sotab)
+    y_sotab = np.array(y_sotab)
+
     print("\nX_sotab shape: ", X_sotab.shape)
     print("y_sotab shape: ", y_sotab.shape)
 
@@ -53,21 +76,17 @@ def prepare_sotab_data_for_inference(data_filepath: str):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--mode", type=str, default="test", help="Mode: train or test.")
+    parser.add_argument("--mode", type=str, default="train", help="Mode: train or test.")
 
-    parser.add_argument("--arts_data", type=str, default="data/arts_top-100-concepts_evidence/arts_evidence.pkl", help="Path to the training and test data synthesized from ARTS.")
+    parser.add_argument("--arts_data", type=str, default="data/arts_mrf_synthesized_data_top-20-concepts/arts_mrf_data.csv", help="Path to the training and test data synthesized from ARTS.")
 
-    parser.add_argument("--num_head_concepts", type=int, default=100, help="Number of head concepts considered in arts data.")
+    parser.add_argument("--num_head_concepts", type=int, default=20, help="Number of head concepts considered in arts data.")
 
-    parser.add_argument("--num_pairs", type=int, default=0, help="Number of concept pairs to consider (0 for considering all pairs).")
-
-    parser.add_argument("--arts_test_size", type=float, default=0.4, help="Test size for the ARTS data.")
+    parser.add_argument("--arts_train_prop", type=float, default=0.6, help="Training proportion of the ARTS data.")
     
     parser.add_argument("--sotab_benchmark", type=str, default="/ssd/congtj/openforge/sotab_v2/artifact/sotab_v2_test_mrf_data.csv", help="Path to the SOTAB benchmark.")
 
     parser.add_argument("--random_seed", type=int, default=12345, help="Random seed.")
-
-    # parser.add_argument("--model_save_filepath", type=str, default="", help="Path to save the trained model")
     
     args = parser.parse_args()
 
@@ -85,43 +104,30 @@ if __name__ == "__main__":
     
     model_save_filepath = os.path.join(exp_dir, "rc_model.pkl")
 
-    with open(arts_data, "rb") as f:
-        arts_data = pickle.load(f)
-        print("Number of arts data points: ", len(arts_data))
-        
-        if args.num_pairs > 0:
-            arts_data = arts_data[:args.num_pairs]
-
-        X_arts = np.array([x[0] for x in arts_data])
-        y_arts = np.array([x[1] for x in arts_data])
-        print("X_arts shape: ", X_arts.shape)
-        print("y_arts shape: ", y_arts.shape)
-
-    X_train, X_test, y_train, y_test = train_test_split(X_arts, y_arts,
-        test_size=args.arts_test_size, random_state=args.random_seed,
-        stratify=y_arts)
-
-    X_valid, X_test, y_valid, y_test = train_test_split(X_test, y_test,
-        test_size=0.5, random_state=args.random_seed, stratify=y_test)
+    X_train, y_train, X_valid, y_valid, X_test, y_test, test_df = load_arts_data(arts_data, args)
 
     if args.mode == "train":
+        print("\nARTS training data shape: ", X_train.shape)
+        print("Number of positive instances in training data: ", np.sum(y_train == 1))
+
         clf = RidgeClassifierwithProba(tol=1e-2, solver="sparse_cg")
         # clf = GaussianProcessClassifier(kernel=1.0*RBF(1.0), random_state=args.random_seed)
         clf.fit(X_train, y_train)
         
         y_valid_pred = clf.predict(X_valid)
         y_test_pred = clf.predict(X_test)
-        print("\nARTS training data shape: ", X_train.shape)
-        print("Number of positive instances in training data: ", np.sum(y_train == 1))
 
         print("\nARTS validation data shape: ", X_valid.shape)
         print("Number of positive instances in validation data: ", np.sum(y_valid == 1))
 
+        print(f"ARTS validation accuracy: {accuracy_score(y_valid, y_valid_pred):.2f}")
+        print(f"ARTS validation F1 score: {f1_score(y_valid, y_valid_pred):.2f}")
+
         print("\nARTS test data shape: ", X_test.shape)
         print("Number of positive instances in test data: ", np.sum(y_test == 1))
-        
-        print(f"\nARTS validation accuracy: {accuracy_score(y_valid, y_valid_pred):.2f}")
+
         print(f"ARTS test accuracy: {accuracy_score(y_test, y_test_pred):.2f}")
+        print(f"ARTS test F1 score: {f1_score(y_test, y_test_pred):.2f}")
 
         test_confdc_scores = clf.predict_proba(X_test)
         print(test_confdc_scores.shape)
@@ -130,33 +136,38 @@ if __name__ == "__main__":
         with open(model_save_filepath, "wb") as f:
             pickle.dump(clf, f)
 
-        arts_confdc_scores_save_filepath = os.path.join(
-            exp_dir, "arts_confidence_scores.pkl"
+        test_df["positive_label_confidence_score"] = test_confdc_scores[:, 1]
+        
+        output_filepath = os.path.join(
+            exp_dir, "arts_test_mrf_data_with_confidence_scores.csv"
         )
-        with open(arts_confdc_scores_save_filepath, "wb") as f:
-            pickle.dump(test_confdc_scores, f)
+        test_df.to_csv(output_filepath, index=False)
     else:
         assert args.mode == "test"
         with open(model_save_filepath, "rb") as f:
             clf = pickle.load(f)
 
+        # ARTS test data evaluation
         y_pred = clf.predict(X_test)
-        print(f"\nARTS F1 score: {f1_score(y_test, y_pred):.2f}")
+        print("ARTS test data shape: ", X_test.shape)
+        print("Number of positive instances in test data: ", np.sum(y_test == 1))
+        print(f"ARTS test accuracy: {accuracy_score(y_test, y_pred):.2f}")
+        print(f"ARTS F1 score: {f1_score(y_test, y_pred):.2f}")
 
         arts_confdc_scores = clf.predict_proba(X_test)
         print("Confidence scores shape: ", arts_confdc_scores.shape)
         print(arts_confdc_scores[:5])
-        print(arts_confdc_scores[:5, 1])
-            
-        X_sotab, y_sotab, sotab_df = prepare_sotab_data_for_inference(args.sotab_benchmark)
+        
+        # SOTAB benchmark evaluation
+        X_sotab, y_sotab, sotab_df = load_sotab_data(args.sotab_benchmark)
 
         y_sotab_pred = clf.predict(X_sotab)
-        print(f"\nSOTAB benchmark F1 score: {f1_score(y_sotab, y_sotab_pred):.2f}")
+        print(f"\nSOTAB benchmark accuracy: {accuracy_score(y_sotab, y_sotab_pred):.2f}")
+        print(f"SOTAB benchmark F1 score: {f1_score(y_sotab, y_sotab_pred):.2f}")
 
         sotab_confdc_scores = clf.predict_proba(X_sotab)
         print("SOTAB confidence scores shape: ", sotab_confdc_scores.shape)
         print(sotab_confdc_scores[:5])
-        print(sotab_confdc_scores[:5, 1])
 
         sotab_df["positive_label_confidence_score"] = sotab_confdc_scores[:, 1]
         
