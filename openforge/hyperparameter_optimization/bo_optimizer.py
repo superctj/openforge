b813e39dc77cd7e24daf9c46d26e105a3435fbb1
superctj <congtj@umich.edu>
2024-02-19 23:44:46 -0500

    complete bayesian-based optimizers

--- openforge/hyperparameter_optimization/bo_optimizer.py
+++ openforge/hyperparameter_optimization/bo_optimizer.py
@@ -1,4 +1,4 @@
-from functools import partial
+from configparser import ConfigParser
 
 from ConfigSpace import ConfigurationSpace
 from smac import BlackBoxFacade as BBFacade
@@ -6,27 +6,25 @@ from smac import HyperparameterOptimizationFacade as HPOFacade
 from smac import Scenario
 
 
-def get_bo_optimizer(config, mrf_hp_space: ConfigurationSpace, target_function):
+def get_bo_optimizer(
+    exp_config: ConfigParser, mrf_hp_space: ConfigurationSpace, target_function
+):
     scenario = Scenario(
         configspace=mrf_hp_space,
-        output_directory=config["results"]["save_path"],
+        output_directory=exp_config["mrf"]["log_dir"],
         deterministic=True,
         objectives="cost",  # minimize the objective
-        n_trials=100,
-        seed=int(config["knob_space"]["random_seed"]),
+        n_trials=exp_config.getint("hp_optimization", "n_trials"),
+        seed=exp_config.getint("hp_optimization", "random_seed"),
     )
 
-    target_function = partial(
-        target_function, seed=int(config["knob_space"]["random_seed"])
-    )
-
-    if config["config_optimizer"]["optimizer"] == "bo-gp":
+    if exp_config.get("hp_optimization", "optimizer") == "bo-gp":
         optimizer = BBFacade(scenario=scenario, target_function=target_function)
-    elif config["config_optimizer"]["optimizer"] == "bo-rf":
+    elif exp_config.get("hp_optimization", "optimizer") == "bo-rf":
         optimizer = HPOFacade(
             scenario=scenario, target_function=target_function
         )
     else:
-        raise ValueError(f"Optimizer {optimizer} not supported.")
+        raise ValueError(f"Optimizer {optimizer} is not supported.")
 
     return optimizer
