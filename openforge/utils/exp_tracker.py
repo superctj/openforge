e53db6764a7d629ce9a78a7dfbf9428e44ce22cf
superctj <congtj@umich.edu>
2024-02-20 16:43:59 -0500

    fix initial value of worst f1 score

--- openforge/utils/exp_tracker.py
+++ openforge/utils/exp_tracker.py
@@ -7,6 +7,6 @@ class ExperimentState:
         self.best_associated_accuracy = -1
         self.best_hp_config = None
 
-        self.worst_f1_score = -1
-        self.worst_associated_accuracy = -1
+        self.worst_f1_score = 1
+        self.worst_associated_accuracy = 1
         self.worst_hp_config = None
