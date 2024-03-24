d14bfc4c03abc5d64770704f2121474dd114cc05
superctj <congtj@umich.edu>
2024-03-24 18:10:31 -0400

    add common utility functions of prior models

---
+++ openforge/utils/prior_model_common.py
@@ -0,0 +1,121 @@
+import logging
+import os
+
+import numpy as np
+import pandas as pd
+
+from sklearn.metrics import accuracy_score, f1_score
+
+
+def load_openforge_sotab_split(
+    split_filepath: str, logger: logging.Logger
+) -> tuple[np.ndarray, np.ndarray, pd.DataFrame]:
+    """Load a split of an OpenForge-SOTAB dataset.
+
+    Args:
+        split_filepath: The file path to the split.
+        logger: The logging instance.
+
+    Returns:
+        X: Features.
+        y: Labels.
+        df: The split as a DataFrame.
+    """
+
+    df = pd.read_csv(split_filepath, delimiter=",", header=0)
+    X, y = [], []
+
+    for row in df.itertuples():
+        X.append(
+            [
+                row.name_qgram_similarity,
+                row.name_jaccard_similarity,
+                row.name_edit_distance,
+                row.name_fasttext_similarity,
+                row.name_word_count_ratio,
+                row.name_char_count_ratio,
+                row.value_jaccard_similarity,
+                row.value_fasttext_similarity,
+            ]
+        )
+        y.append(row.relation_variable_label)
+
+    X = np.array(X)
+    y = np.array(y)
+
+    logger.info(f"\nX shape: {X.shape}")
+    logger.info(f"y shape: {y.shape}")
+    logger.info(f"Number of positive instances: {np.sum(y == 1)}")
+
+    return X, y, df
+
+
+def load_openforge_sotab_small(data_dir: str, logger: logging.Logger) -> tuple[
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    pd.DataFrame,
+    pd.DataFrame,
+    pd.DataFrame,
+]:
+    """Load training, validation, and test splits of the OpenForge-SOTAB-Small
+    dataset.
+
+    Args:
+        data_dir: The directory containing the benchmark.
+        logger: The logging instance.
+
+    Returns:
+        The training, validation, and test data.
+    """
+
+    train_filepath = os.path.join(data_dir, "training.csv")
+    valid_filepath = os.path.join(data_dir, "validation.csv")
+    test_filepath = os.path.join(data_dir, "test.csv")
+
+    logger.info("Loading training split...")
+    X_train, y_train, train_df = load_openforge_sotab_split(
+        train_filepath, logger
+    )
+
+    logger.info("Loading validation split...")
+    X_valid, y_valid, valid_df = load_openforge_sotab_split(
+        valid_filepath, logger
+    )
+
+    logger.info("Loading test split...")
+    X_test, y_test, test_df = load_openforge_sotab_split(test_filepath, logger)
+
+    return (
+        X_train,
+        y_train,
+        X_valid,
+        y_valid,
+        X_test,
+        y_test,
+        train_df,
+        valid_df,
+        test_df,
+    )
+
+
+def log_exp_metrics(
+    split: str, y: np.ndarray, y_pred: np.ndarray, logger: logging.Logger
+):
+    """Log experiment metrics of a dataset split.
+
+    Args:
+        y: The true labels.
+        y_pred: The predicted labels.
+        logger: The logging instance.
+    """
+
+    accuracy = accuracy_score(y, y_pred)
+    f1 = f1_score(y, y_pred)
+
+    logger.info(f"Split: {split}")
+    logger.info(f"  Accuracy: {accuracy}")
+    logger.info(f"  F1 score: {f1}")
