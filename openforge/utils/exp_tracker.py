0cd01a25602fb62c2ba230896776778893ddd035
superctj <congtj@umich.edu>
2024-03-24 22:36:07 -0400

    log best hp config

--- openforge/utils/exp_tracker.py
+++ openforge/utils/exp_tracker.py
@@ -1,3 +1,8 @@
+import logging
+
+from ConfigSpace import ConfigurationSpace
+
+
 class ExperimentState:
     def __init__(self):
         self.default_f1_score = -1
@@ -10,3 +15,22 @@ class ExperimentState:
         self.worst_f1_score = 1
         self.worst_associated_accuracy = 1
         self.worst_hp_config = None
+
+    def log_best_hp_config(
+        self, best_hp_config: ConfigurationSpace, logger: logging.Logger
+    ):
+        logger.info("\nCompleted hyperparamter optimization.")
+
+        logger.info(f"\nBest hyperparameter configuration:\n{best_hp_config}")
+        logger.info(f"Best F1 score: {self.best_f1_score:.2f}")
+        logger.info(f"Associated accuracy: {self.best_associated_accuracy:.2f}")
+
+        logger.info(
+            f"\nWorst hyperparameter configuration:\n{self.worst_hp_config}"
+        )
+        logger.info(f"Worst F1 score: {self.worst_f1_score:.2f}")
+        logger.info(
+            f"Associated accuracy: {self.worst_associated_accuracy:.2f}"
+        )
+
+        assert dict(best_hp_config) == dict(self.best_hp_config)
