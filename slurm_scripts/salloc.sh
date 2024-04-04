fd47d98c06e29488d0de62471864f112cb04afae
superctj <congtj@umich.edu>
2024-04-04 00:18:58 -0400

    add partition flag

--- slurm_scripts/salloc.sh
+++ slurm_scripts/salloc.sh
@@ -1,1 +1,1 @@
-salloc --account=jag0 --nodes=1 --ntasks-per-node=1 --cpus-per-task=4 --mem=64g
+salloc --account=jag0 --partition=largemem --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --mem=128g
