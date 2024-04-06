c85b96d005c33e856c7ae0bc2399d39487a0b290
superctj <congtj@umich.edu>
2024-04-06 00:01:23 -0400

    add data loading apis for openforge-arts benchmark

--- openforge/utils/prior_model_common.py
+++ openforge/utils/prior_model_common.py
@@ -207,6 +207,103 @@ def load_openforge_icpsr_benchmark(
     )
 
 
+def load_openforge_arts_split(
+    split_filepath: str, logger: logging.Logger
+) -> tuple[np.ndarray, np.ndarray, pd.DataFrame]:
+    """Load a split of an OpenForge-ARTS dataset.
+
+    Args:
+        split_filepath: The file path to the split.
+        logger: The logging instance.
+
+    Returns:
+        X: Features.
+        y: Labels.
+        df: The split as a DataFrame.
+    """
+
+    df = pd.read_csv(split_filepath, delimiter=",", header=0)
+    X, y = [], []
+
+    for row in df.itertuples():
+        X.append(
+            [
+                row.name_qgram_similarity,
+                row.name_jaccard_similarity,
+                row.name_edit_distance,
+                row.name_fasttext_similarity,
+                row.name_word_count_ratio,
+                row.name_char_count_ratio,
+                row.value_jaccard_similarity,
+                row.value_fasttext_similarity,
+            ]
+        )
+        y.append(row.relation_variable_label)
+
+    X = np.array(X)
+    y = np.array(y)
+
+    logger.info(f"X shape: {X.shape}")
+    logger.info(f"y shape: {y.shape}")
+    logger.info(f"Number of positive instances: {np.sum(y == 1)}\n")
+
+    return X, y, df
+
+
+def load_openforge_arts_benchmark(
+    data_dir: str, logger: logging.Logger
+) -> tuple[
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    pd.DataFrame,
+    pd.DataFrame,
+    pd.DataFrame,
+]:
+    """Load training, validation, and test splits of the OpenForge-ARTS
+    benchmark.
+
+    Args:
+        data_dir: The directory containing the benchmark.
+        logger: The logging instance.
+
+    Returns:
+        The training, validation, and test data.
+    """
+
+    train_filepath = os.path.join(data_dir, "openforge_arts_training.csv")
+    valid_filepath = os.path.join(data_dir, "openforge_arts_validation.csv")
+    test_filepath = os.path.join(data_dir, "openforge_arts_test.csv")
+
+    logger.info("Loading training split...")
+    X_train, y_train, train_df = load_openforge_arts_split(
+        train_filepath, logger
+    )
+
+    logger.info("Loading validation split...")
+    X_valid, y_valid, valid_df = load_openforge_arts_split(
+        valid_filepath, logger
+    )
+
+    logger.info("Loading test split...")
+    X_test, y_test, test_df = load_openforge_arts_split(test_filepath, logger)
+
+    return (
+        X_train,
+        y_train,
+        X_valid,
+        y_valid,
+        X_test,
+        y_test,
+        train_df,
+        valid_df,
+        test_df,
+    )
+
+
 def evaluate_prior_model_predictions(
     y: np.ndarray, y_pred: np.ndarray, multi_class: bool = False
 ) -> tuple[float, float]:
