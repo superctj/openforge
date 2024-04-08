0a258240d834834567f0a8854258e08ba2a2469e
superctj <congtj@umich.edu>
2024-04-08 01:07:46 -0400

    update slurm commands

--- slurm_scripts/evaluate_gpu_lbp_scalability.sh
+++ slurm_scripts/evaluate_gpu_lbp_scalability.sh
@@ -4,19 +4,19 @@
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=64gb
-#SBATCH --time=3-00:00
-#SBATCH --account=jag0
-#SBATCH --output=/home/congtj/openforge/logs/synthesized_mrf
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128g
+#SBATCH --time=1-00:00
+#SBATCH --account=jag98
+#SBATCH --output=/home/congtj/openforge/logs/synthesized_mrf/evaluate_gpu_lbp_scalability.outputs
 
 # Stop on errors
 set -Eeuo pipefail
 
-source ~/.conda/envs/openforge-pgmax-gpu/bin/activate
-conda activate openforge-pgmax-gpu
+source /home/congtj/.bash_profile
+conda activate pgmax-gpu
 
-for n in $(seq 200 200 1000)
+for n in $(seq 20 20 100)
 do
     python openforge/evaluate_lbp_efficiency.py --num_concepts=${n}
 done
