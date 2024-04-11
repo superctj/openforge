bf8d267e8113f2a200ed81753a0447912d98293e
superctj <congtj@umich.edu>
2024-04-11 00:34:33 -0400

    add slurm script for running hp tunning on arts-multi

---
+++ slurm_scripts/arts_multi_pgmax_gpu_lbp_hp_tuning.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=arts-hyper_pgmax-gpu-lbp_hp-tuning
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128g
+#SBATCH --time=1-00:00:00
+#SBATCH --account=jag98
+#SBATCH --output=/home/congtj/openforge/openforge/mrf_inference/arts_multi_outputs.log
+
+module load cuda/12.1.1
+source ~/.bashrc
+conda activate pgmax-gpu
+
+python ./openforge/mrf_inference/pgmax_lbp_arts_multi.py \
+    --config_path=./openforge/mrf_inference/tuning_exp_configs/arts_multi_rf_pgmax_lbp.ini \
+    --mode=hp_tuning
