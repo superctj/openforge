61e2f642232113e9e1a02857d562ed36b6a86918
superctj <congtj@umich.edu>
2024-04-13 14:09:11 -0400

    run inference for best config

--- slurm_scripts/arts_multi_pgmax_gpu_lbp_hp_tuning.sh
+++ slurm_scripts/arts_multi_pgmax_gpu_lbp_hp_tuning.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=128g
-#SBATCH --time=1-00:00:00
+#SBATCH --time=01:00:00
 #SBATCH --account=jag98
 #SBATCH --output=/home/congtj/openforge/openforge/mrf_inference/arts_multi_rf_outputs.log
 
@@ -16,4 +16,4 @@ conda activate pgmax-gpu
 
 python ./openforge/mrf_inference/pgmax_lbp_arts_multi.py \
     --config_path=./openforge/mrf_inference/tuning_exp_configs/arts_multi_rf_pgmax_lbp.ini \
-    --mode=hp_tuning
+    --mode=inference
