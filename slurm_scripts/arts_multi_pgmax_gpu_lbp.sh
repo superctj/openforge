8bbb636a597bb0d0c3246681d58a3e18def48762
superctj <congtj@umich.edu>
2024-04-15 00:12:47 -0400

    run pgmax-lbp tuning with ridge prior on arts-multi

--- slurm_scripts/arts_multi_pgmax_gpu_lbp.sh
+++ slurm_scripts/arts_multi_pgmax_gpu_lbp.sh
@@ -6,14 +6,14 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=128g
-#SBATCH --time=01:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --account=jag98
-#SBATCH --output=/home/congtj/openforge/openforge/mrf_inference/arts_multi_rf_outputs.log
+#SBATCH --output=/home/congtj/openforge/openforge/mrf_inference/arts_multi_gbdt_pgmax_gpu_lbp_hp_tuning.log
 
 module load cuda/12.1.1
 source ~/.bashrc
 conda activate pgmax-gpu
 
 python ./openforge/mrf_inference/pgmax_lbp_arts_multi.py \
-    --config_path=./openforge/mrf_inference/tuning_exp_configs/arts_multi_rf_pgmax_lbp.ini \
-    --mode=inference
+    --config_path=./openforge/mrf_inference/tuning_exp_configs/arts_multi_gbdt_pgmax_lbp.ini \
+    --mode=hp_tuning
