85e228b2126c4cc850ada5fb6e018c569f1f3845
superctj <congtj@umich.edu>
2024-04-17 21:30:21 -0400

    add latest experiments

--- slurm_scripts/arts_multi_pgmax_gpu_lbp.sh
+++ slurm_scripts/arts_multi_pgmax_gpu_lbp.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=arts-multi_pgmax-gpu-lbp_hp-tuning
+#SBATCH --job-name=arts-multi_pgmax-gpu-lbp_inference
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=128g
-#SBATCH --time=1-00:00:00
+#SBATCH --time=01:00:00
 #SBATCH --account=jag98
-#SBATCH --output=/home/congtj/openforge/openforge/mrf_inference/arts_multi_gbdt_pgmax_gpu_lbp_hp_tuning.log
+#SBATCH --output=/home/congtj/openforge/openforge/mrf_inference/arts_multi_gbdt_pgmax_gpu_lbp_inference.log
 
 module load cuda/12.1.1
 source ~/.bashrc
@@ -16,4 +16,4 @@ conda activate pgmax-gpu
 
 python ./openforge/mrf_inference/pgmax_lbp_arts_multi.py \
     --config_path=./openforge/mrf_inference/tuning_exp_configs/arts_multi_gbdt_pgmax_lbp.ini \
-    --mode=hp_tuning
+    --mode=inference
