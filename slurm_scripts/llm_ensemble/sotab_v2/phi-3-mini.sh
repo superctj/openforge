9577d8424e1fad97805039aa1f5f510bfbd5b906
superctj <congtj@umich.edu>
2024-09-17 17:32:49 -0400

    add slurm script for phi-3-mini exps

---
+++ slurm_scripts/llm_ensemble/sotab_v2/phi-3-mini.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=phi-3-mini_predictions
+#SBATCH --partition=spgpu
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64g
+#SBATCH --time=02:00:00
+#SBATCH --account=jag0
+
+source ~/.bash_profile
+conda activate pgmax-gpu
+
+python ./openforge/llm_ensemble/msft_phi3.py \
+    --config_path=./openforge/llm_ensemble/exp_configs/sotab-v2_10-shots/phi-3-mini.ini  \
+    --mode=inference
