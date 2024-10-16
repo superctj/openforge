ab885fbbe7b544bee05f3ed7d3caf79dc7fe2999
superctj <congtj@umich.edu>
2024-10-16 10:24:03 -0400

    add slurm script for prompting gemma-2 on sotab-v2

--- slurm_scripts/llm/sotab_v2/gemma-2-9b-it.sh
+++ slurm_scripts/llm/sotab_v2/gemma-2-9b-it.sh
@@ -4,14 +4,14 @@
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=64g
 #SBATCH --time=03:00:00
 #SBATCH --account=jag0
 
 source ~/.bash_profile
-conda activate openforge-gpu
+conda activate huggingface
 
 python ./openforge/llm_ensemble/msft_phi3.py \
-    --config_path=./openforge/llm_ensemble/exp_configs/sotab-v2_10-shots/gemma-2-9b-it.ini  \
+    --config_path=./openforge/llm_ensemble/exp_configs/sotab-v2_0-shot/gemma-2-9b-it.ini  \
     --mode=inference
