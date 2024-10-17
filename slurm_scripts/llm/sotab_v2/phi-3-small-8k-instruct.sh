8af3e0decb6bf7356a655496ef381672b4291746
superctj <congtj@umich.edu>
2024-10-17 10:26:27 -0400

    add exp configs and slurm scripts for phi-3-small experiments

--- slurm_scripts/llm/sotab_v2/phi-3-small-8k-instruct.sh
+++ slurm_scripts/llm/sotab_v2/phi-3-small-8k-instruct.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=phi-3-small-8k-instruct_predictions
+#SBATCH --job-name=phi-3-small-8k-instruct
 #SBATCH --partition=spgpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=64g
 #SBATCH --time=03:00:00
 #SBATCH --account=jag0
 
 source ~/.bash_profile
-conda activate pgmax-gpu
+conda activate huggingface
 
 python ./openforge/llm_ensemble/msft_phi3.py \
-    --config_path=./openforge/llm_ensemble/exp_configs/sotab-v2_10-shots/phi-3-small.ini  \
+    --config_path=./openforge/llm_ensemble/exp_configs/sotab-v2_0-shot/phi-3-small-8k-instruct.ini  \
     --mode=inference
