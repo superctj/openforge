1c0500f7cc04b652e204233355d3ac82f74022d6
superctj <congtj@umich.edu>
2024-10-22 10:25:49 -0400

    add latest salloc command

--- slurm_scripts/salloc.sh
+++ slurm_scripts/salloc.sh
@@ -1,3 +1,5 @@
 salloc --account=jag0 --partition=largemem --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --mem=128g
 
 salloc --account=jag98 --partition=gpu --gres=gpu:1 --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --mem=128g --time=05:00:00
+
+salloc --account=jag0 --partition=spgpu --gres=gpu:1 --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --mem=64g --time=4:00:00
