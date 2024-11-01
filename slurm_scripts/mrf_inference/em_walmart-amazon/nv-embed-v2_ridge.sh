67292a7b08b755f905c6b17c16e5ec307c6875c6
superctj <congtj@umich.edu>
2024-11-01 15:34:13 -0400

    test inference with fixed lbp hyperparameters

--- slurm_scripts/mrf_inference/em_walmart-amazon/nv-embed-v2_ridge.sh
+++ slurm_scripts/mrf_inference/em_walmart-amazon/nv-embed-v2_ridge.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64g
-#SBATCH --time=10:00:00
+#SBATCH --time=1:00:00
 #SBATCH --account=jag0
 
 source ~/.bash_profile
@@ -15,4 +15,4 @@ export LD_LIBRARY_PATH=$CONDA_PREFIX/lib
 
 python ./openforge/mrf_inference/pgmax_lbp_em_wa.py \
     --config_path=./openforge/mrf_inference/tuning_exp_configs/em_walmart-amazon/nv-embed-v2_ridge.ini \
-    --mode=hp_tuning
+    --mode=inference
