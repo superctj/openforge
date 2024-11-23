0acda38a24b323ada8e406d54fb9340e88fdda1a
superctj <congtj@umich.edu>
2024-11-23 14:52:44 -0500

    exp with gemma-2 prior for mrf inference on walmart-amazon dataset

---
+++ slurm_scripts/mrf_inference/em_walmart-amazon/nv-embed-v2_gemma-2-9b-it.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=mrf-inference_nv-embed-v2_gemma-2-9b-it_cpu
+#SBATCH --partition=largemem
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=16g
+#SBATCH --time=8:00:00
+#SBATCH --account=jag0
+
+source ~/.bash_profile
+conda activate pgmax-cpu
+
+python ./openforge/mrf_inference/pgmax_lbp_em_wa_cpu.py \
+    --config_path=./openforge/mrf_inference/tuning_exp_configs/em_walmart-amazon/nv-embed-v2_gemma-2-9b-it_cpu.ini \
+    --mode=hp_tuning
