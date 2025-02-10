5b92974e106a25f844163631b9e6573034a35b68
superctj <congtj@umich.edu>
2025-02-09 20:47:26 -0500

    add slurm scripts for running pgmax lbp inference and hp tuning on icpsr-hyper-hypo dataset

---
+++ slurm_scripts/mrf_inference/icpsr_hyper_hypo/gemma-2-9b-it-lora.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=mrf-inference_gemma-2-9b-it-lora
+#SBATCH --partition=spgpu
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128g
+#SBATCH --time=2:00:00
+#SBATCH --account=jag0
+
+source ~/.bash_profile
+conda activate pgmax-gpu
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib
+
+python ./openforge/mrf_inference/pgmax_lbp_icpsr_hyper.py \
+    --config_path=./openforge/mrf_inference/tuning_exp_configs/icpsr/gemma-2-9b-it-lora.ini \
+    --mode=hp_tuning
