61964181c9114c2cd9cf91af9a99505adf3d1ad4
superctj <congtj@umich.edu>
2025-02-13 20:26:40 -0500

    add exp configs and slurm scripts for running mrf inference with llm prompting prior

--- slurm_scripts/mrf_inference/icpsr_hyper_hypo/qwen2.5-7b-instruct.sh
+++ slurm_scripts/mrf_inference/icpsr_hyper_hypo/qwen2.5-7b-instruct.sh
@@ -6,13 +6,13 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=128g
-#SBATCH --time=2:00:00
+#SBATCH --time=4:00:00
 #SBATCH --account=jag0
 
 source ~/.bash_profile
 conda activate pgmax-gpu
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib
 
-python ./openforge/mrf_inference/pgmax_lbp_icpsr_hyper.py \
-    --config_path=./openforge/mrf_inference/tuning_exp_configs/icpsr/10_shots/qwen2.5-7b-instruct.ini \
+python ./openforge/mrf_inference/pgmax_lbp_icpsr_hyper_hypo.py \
+    --config_path=./openforge/mrf_inference/tuning_exp_configs/icpsr_hyper_hypo/0_shot/qwen2.5-7b-instruct.ini \
     --mode=hp_tuning
