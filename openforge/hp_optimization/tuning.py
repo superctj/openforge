8a4a455207ba1d43ceedba4ed9974545d00d3162
superctj <congtj@umich.edu>
2025-03-05 22:20:52 -0500

    add tuning engine for two-stage binary classification

--- openforge/hp_optimization/tuning.py
+++ openforge/hp_optimization/tuning.py
@@ -8,6 +8,7 @@ from openforge.utils.exp_tracker import ExperimentState
 from openforge.utils.mrf_common import (
     evaluate_inference_results,
     evaluate_multi_class_inference_results,
+    evaluate_two_stage_inference_results,
 )
 from openforge.utils.prior_model_common import (
     evaluate_prior_model_predictions,
@@ -173,6 +174,53 @@ class PriorModelTuningEngine:
             self.exp_state.worst_f1_score = f1_score
             self.exp_state.worst_associated_accuracy = accuracy
             self.exp_state.worst_hp_config = hp_config
+
+        return -f1_score
+
+    def run(self):
+        best_hp_config = self.optimizer.optimize()
+
+        self.exp_state.log_best_hp_config(best_hp_config, self.logger)
+
+        return best_hp_config
+
+
+class TwoStageTuningEngine:
+    def __init__(
+        self, exp_config, mrf_wrapper, mrf_hp_space, stage
+    ):
+        self.exp_config = exp_config
+        self.mrf_wrapper = mrf_wrapper
+        self.mrf_hp_space = mrf_hp_space
+        self.stage = stage
+
+        self.optimizer = get_bo_optimizer(
+            self.exp_config, self.mrf_hp_space, self.bo_target_function
+        )
+        self.exp_state = ExperimentState()
+        self.logger = get_logger()
+
+    def bo_target_function(self, mrf_hp_config: ConfigurationSpace, seed: int):
+        """
+        Target function for Bayesian Optimization.
+        """
+
+        mrf = self.mrf_wrapper.create_mrf(dict(mrf_hp_config))
+        results = self.mrf_wrapper.run_inference(mrf, dict(mrf_hp_config))
+
+        f1_score, accuracy, _, _ = evaluate_two_stage_inference_results(
+            self.mrf_wrapper.prior_data, results, stage=self.stage
+        )
+
+        if self.exp_state.best_f1_score < f1_score:
+            self.exp_state.best_f1_score = f1_score
+            self.exp_state.best_associated_accuracy = accuracy
+            self.exp_state.best_hp_config = mrf_hp_config
+
+        if self.exp_state.worst_f1_score > f1_score:
+            self.exp_state.worst_f1_score = f1_score
+            self.exp_state.worst_associated_accuracy = accuracy
+            self.exp_state.worst_hp_config = mrf_hp_config
 
         return -f1_score
 
